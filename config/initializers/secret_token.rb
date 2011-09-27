72230e0a18e924c2774e42749c98126c454661a3
Jelmer <jelmer@jelmer-desktop.(none)>
2011-09-27 10:00:41 +0200

    Start peugeotvelgen

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peugeotvelgen::Application.config.secret_token = '458010bda589e7fad1557e6dd4921904fb0300cf7ac3cc49354b7c88ed7b9bff359493adea491cedea9f73f20f81a65ae0efa2c32b55c8a98aa2da1011cdeecb'
